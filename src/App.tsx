aa9b37254c6e57817700ee06e9d057ca8880756a
Roman Sereda <roman.sereda@pm.me>
2020-10-22 02:27:56 +0300

    Setup project, add some pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss';
+import Header from "../../components/Header/Header";
+import SideMenu from "../../components/SideMenu/SideMenu";
+import Footer from "../../components/Footer/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+
+const App = () => {
+
+  return(
+    <div>
+      <Header />
+      <div className={styles.content}>
+        <SideMenu />
+
+      </div>
+      <Footer />
     </div>
-  );
-}
+  )
+};
 
 export default App;
+
